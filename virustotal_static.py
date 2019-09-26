57614d0bd113e0cbd379a5e2e6086bc4264cd4e9
Angela Deaguiar <angela.deaguiar@165gc.onmicrosoft.com>
2019-09-26 08:09:46 -0300

    Fixed bugs

--- virustotal_static.py
+++ virustotal_static.py
@@ -1,9 +1,8 @@
 import json
+import requests
 
-
-from assemblyline.al.common.result import Result, ResultSection, Classification, SCORE, TEXT_FORMAT
-from assemblyline.al.common.av_result import VirusHitTag
-from assemblyline.al.service.base import ServiceBase
+from assemblyline_v4_service.common.base import ServiceBase
+from assemblyline_v4_service.common.result import Result, ResultSection, Classification, BODY_FORMAT
 
 
 class VTException(Exception):
@@ -15,35 +14,34 @@ class VTException(Exception):
 
 
 class AvHitSection(ResultSection):
-    def __init__(self, av_name, virus_name, score):
+    def __init__(self, av_name, virus_name):
         title = '%s identified the file as %s' % (av_name, virus_name)
         super(AvHitSection, self).__init__(
             title_text=title,
-            score=score,
             classification=Classification.UNRESTRICTED)
 
 
 class VirusTotalStatic(ServiceBase):
-    SERVICE_CATEGORY = "External"
-    SERVICE_DESCRIPTION = "This service checks the file hash to see if there's an existing VirusTotal report."
-    SERVICE_ENABLED = False
-    SERVICE_REVISION = ServiceBase.parse_revision('$Id$')
-    SERVICE_STAGE = "CORE"
-    SERVICE_TIMEOUT = 60
-    SERVICE_IS_EXTERNAL = True
-    SERVICE_DEFAULT_CONFIG = {
-        'API_KEY': '',
-        'BASE_URL': 'https://www.virustotal.com/vtapi/v2/'
-    }
+#    SERVICE_CATEGORY = "External"
+#    SERVICE_DESCRIPTION = "This service checks the file hash to see if there's an existing VirusTotal report."
+#    SERVICE_ENABLED = False
+#    SERVICE_REVISION = ServiceBase.parse_revision('$Id$')
+#    SERVICE_STAGE = "CORE"
+#    SERVICE_TIMEOUT = 60
+#    SERVICE_IS_EXTERNAL = True
+#    SERVICE_DEFAULT_CONFIG = {
+#        'API_KEY': '',
+#        'BASE_URL': 'https://www.virustotal.com/vtapi/v2/'
+#    }
 
-    def __init__(self, cfg=None):
-        super(VirusTotalStatic, self).__init__(cfg)
-        self.api_key = self.cfg.get('API_KEY')
+    def __init__(self, config=None):
+        super(VirusTotalStatic, self).__init__(config)
+        self.api_key = self.config.get("api_key", None)
 
     # noinspection PyGlobalUndefined,PyUnresolvedReferences
-    def import_service_deps(self):
-        global requests
-        import requests
+#    def import_service_deps(self):
+ #       global requests
+  #      import requests
 
     def start(self):
         self.log.debug("VirusTotalStatic service started")
@@ -56,7 +54,7 @@ class VirusTotalStatic(ServiceBase):
     def scan_file(self, request):
 
         # Check to see if the file has been seen before
-        url = self.cfg.get('BASE_URL') + "file/report"
+        url = self.config.get("base_url") + "file/report"
         params = {'apikey': self.api_key, 'resource': request.sha256}
         r = requests.post(url, params)
         try:
@@ -74,22 +72,24 @@ class VirusTotalStatic(ServiceBase):
         response = response.get('results', response)
 
         if response is not None and response.get('response_code') == 1:
-            av_hits = ResultSection(title_text='Anti-Virus Detections')
             url_section = ResultSection(
-                SCORE.NULL,
                 'Virus total report permalink',
-                self.SERVICE_CLASSIFICATION,
-                body_format=TEXT_FORMAT.URL,
+                body_format=BODY_FORMAT.URL,
                 body=json.dumps({"url": response.get('permalink')}))
             res.add_section(url_section)
 
             scans = response.get('scans', response)
+            av_hits = ResultSection(title_text='Anti-Virus Detections')
             av_hits.add_line('Found %d AV hit(s) from %d scans.' % (response.get('positives'), response.get('total')))
-            for majorkey, subdict in sorted(scans.iteritems()):
+            for majorkey, subdict in sorted(scans.items()):
                 if subdict['detected']:
                     virus_name = subdict['result']
-                    res.append_tag(VirusHitTag(virus_name, context="scanner:%s" % majorkey))
-                    av_hits.add_section(AvHitSection(majorkey, virus_name, SCORE.SURE))
-            res.add_result(av_hits)
+                    av_hit_section = AvHitSection(majorkey, virus_name)
+                    av_hit_section.set_heuristic(1, signature=f'{majorkey}.{virus_name}')
+                    av_hit_section.add_tag('av.virus_name', virus_name)
+                    av_hits.add_subsection(av_hit_section)
+
+            res.add_section(av_hits)
 
         return res
+
